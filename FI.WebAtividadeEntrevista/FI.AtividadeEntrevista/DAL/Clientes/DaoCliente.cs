3004f05fe0d31a604fdb532589848b1f6579b50c
Felipe <felipe.monteiro2626@gmail.com>
2024-03-19 14:54:24 -0300

    feat(Entrevista): incluindo funcao cpf atualizar

--- FI.WebAtividadeEntrevista/FI.AtividadeEntrevista/DAL/Clientes/DaoCliente.cs
+++ FI.WebAtividadeEntrevista/FI.AtividadeEntrevista/DAL/Clientes/DaoCliente.cs
@@ -121,6 +121,8 @@ namespace FI.AtividadeEntrevista.DAL
             parametros.Add(new System.Data.SqlClient.SqlParameter("Email", cliente.Email));
             parametros.Add(new System.Data.SqlClient.SqlParameter("Telefone", cliente.Telefone));
             parametros.Add(new System.Data.SqlClient.SqlParameter("ID", cliente.Id));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Cpf", cliente.Cpf));
+
 
             base.Executar("FI_SP_AltCliente", parametros);
         }
@@ -157,6 +159,7 @@ namespace FI.AtividadeEntrevista.DAL
                     cli.Nome = row.Field<string>("Nome");
                     cli.Sobrenome = row.Field<string>("Sobrenome");
                     cli.Telefone = row.Field<string>("Telefone");
+                    cli.Cpf = row.Field<string>("CPF");
                     lista.Add(cli);
                 }
             }
