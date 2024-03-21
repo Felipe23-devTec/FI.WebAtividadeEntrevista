b2de401d8bb26ba1b7387407ace9ff101513980b
Felipe <felipe.monteiro2626@gmail.com>
2024-03-21 20:00:14 -0300

    correcoes de controller

--- FI.WebAtividadeEntrevista/FI.WebAtividadeEntrevista/Controllers/BeneficiarioController.cs
+++ FI.WebAtividadeEntrevista/FI.WebAtividadeEntrevista/Controllers/BeneficiarioController.cs
@@ -23,40 +23,5 @@ namespace WebAtividadeEntrevista.Controllers
         {
             return View();
         }
-
-        [HttpPost]
-        public JsonResult BeneficiarioList()
-        {
-            var result = new RequestActionResult() { isOk = false };
-            try
-            {
-                List<Beneficiario> beneficiarios = new BoBeneficiario().Consultar();
-                result.isOk = true;
-                result.bag = beneficiarios;
-                return Json(result, JsonRequestBehavior.AllowGet);
-            }
-            catch (Exception ex)
-            {
-                return Json(new { Result = "ERROR", Message = ex.Message });
-            }
-        }
-
-        [HttpPost]
-        public JsonResult Excluir(string Cpf)
-        {
-            var result = new RequestActionResult() { isOk = false };
-            try
-            {
-                BoBeneficiario ben = new BoBeneficiario();
-                ben.Excluir(Cpf);
-                result.isOk = true;
-                result.message = "Exclusao com sucesso!";
-                return Json(result, JsonRequestBehavior.AllowGet);
-            }
-            catch (Exception ex)
-            {
-                return Json(new { Result = "ERROR", Message = ex.Message });
-            }
-        }
     }
 }
